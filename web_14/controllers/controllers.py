14f5a29d28b09dda4a985611c101b7dc7ee4ec5c
Livingston <livin.arias88@gmail.com>
2022-08-23 10:56:43 -0500

    web introdoo

---
+++ web_14/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class Web14(http.Controller):
+     @http.route('/web_14/introdoo/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
+
+#     @http.route('/web_14/web_14/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('web_14.listing', {
+#             'root': '/web_14/web_14',
+#             'objects': http.request.env['web_14.web_14'].search([]),
+#         })
+
+#     @http.route('/web_14/web_14/objects/<model("web_14.web_14"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('web_14.object', {
+#             'object': obj
+#         })
